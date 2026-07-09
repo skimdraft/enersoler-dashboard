95d6ebbdb888252fe0c8506e727f5d858672f416
Enersoler Dashboard <contact@enersoler.com>
2026-07-09 11:10:44 -1000

    cleanup: remove accidentally committed local files

--- find-content.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const d = fs.readFileSync('docs/dashboard.html', 'utf8');
-const idx = d.indexOf('id="content"');
-console.log('Content div at', idx);
-if (idx >= 0) console.log(d.substring(idx, idx + 600));
